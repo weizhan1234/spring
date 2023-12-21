3c394c263977dd0c5676b0d11619e34022155ec0
JBlazingSun <blazings@vip.qq.com>
2023-12-21 12:14:31 +0800

    add files

--- spring/src/main/java/org/example/spring/Application.java
+++ spring/src/main/java/org/example/spring/Application.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
+@RestController
 public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
